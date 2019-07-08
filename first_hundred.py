f8c60c3218312a96a737cc09bb4aac0ae8fba3da
Haochuan Wei <haochuanwei@yahoo.com>
2019-07-08 10:22:17 +0800

    Finished the 22th problem; added a config file for attachment management (#19)

--- first_hundred.py
+++ first_hundred.py
@@ -992,8 +992,27 @@ def Euler_Problem_21(n=10000):
                 amicables += [a, b]
     return amicables        
 
+def Euler_Problem_22():
+    '''
+    Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
 
+    For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 * 53 = 49714.
 
+    What is the total of all the name scores in the file?
+    '''
+    from custom_config import get_attachment_path 
+    with open(get_attachment_path(22), 'r') as f:
+        names = sorted([eval(_x) for _x in f.read().split(',')])
+    char_to_int = {
+            'A':  1, 'B':  2, 'C':  3, 'D':  4, 'E':  5, 'F':  6, 'G':  7,
+            'H':  8, 'I':  9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14,
+            'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21,
+            'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26
+            }
 
+    def name_to_score(name):
+        return sum([char_to_int[_char] for _char in list(name)])
 
+    scores = [(i+1) * name_to_score(_name) for i, _name in enumerate(names)]
+    return sum(scores)
 
