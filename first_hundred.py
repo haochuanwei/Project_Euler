b20a821850263db976fa3095c642b465ec0738c7
Haochuan Wei <haochuanwei@yahoo.com>
2019-07-22 09:31:56 +0800

    Finished the 40th problem (#38)

--- first_hundred.py
+++ first_hundred.py
@@ -1417,3 +1417,30 @@ def Euler_Problem_39(max_perimeter=1000):
             solution_count[perimeter] += 1
     return max(list(solution_count.keys()), key=lambda x: solution_count[x])
 
+def Euler_Problem_40():
+    '''
+    An irrational decimal fraction is created by concatenating the positive integers:
+    0.123456789101112131415161718192021...
+    It can be seen that the 12th digit of the fractional part is 1.
+    If dn represents the nth digit of the fractional part, find the value of the following expression.
+    d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+    '''
+    def get_n_digits(n):
+        '''
+        Compute n digits of the fractional part of the irrational number.
+        '''
+        retarr = []
+        num = 0
+        while len(retarr) < n:
+            num += 1
+            retarr += list(map(int, list(str(num))))
+        return retarr
+   
+    # brute-force the first 1000000 digits
+    arr = get_n_digits(1000000)
+    indices_plus_one = [1, 10, 100, 1000, 10000, 100000, 1000000]
+    retval = 1
+    for _i in indices_plus_one:
+        retval *= arr[_i - 1]
+    return retval
+
