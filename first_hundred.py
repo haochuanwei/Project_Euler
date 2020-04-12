113b1afdc51307be6d9ee4c5001e2a95344f4d17
Haochuan Wei <haochuanwei@yahoo.com>
2020-04-12 12:26:00 +0800

    Finished the 97th problem

--- first_hundred.py
+++ first_hundred.py
@@ -3665,6 +3665,27 @@ def euler_problem_95(bound=round(1e+6)):
             
 
 @wrappy.probe()
+def euler_problem_97(digits=10):
+    '''
+    The first known prime found to exceed one million digits was discovered in 1999, and is a Mersenne prime of the form 2^6972593 − 1; it contains exactly 2,098,960 digits. Subsequently other Mersenne primes, of the form 2^p − 1, have been found which contain more digits.
+
+    However, in 2004 there was found a massive non-Mersenne prime which contains 2,357,207 digits: 28433×2^7830457 + 1.
+
+    Find the last ten digits of this prime number.
+    '''
+    # idea: we can discard all digits beyond the last ten.
+    from subroutines import IntegerModulos
+    
+    assert isinstance(digits, int)
+    num = IntegerModulos(28433, round(10 ** digits))
+    for i in tqdm(range(7830457)):
+        num *= 2
+        
+    num += 1
+    return num.value()
+
+
+@wrappy.probe()
 def euler_problem_99():
     '''
     Comparing two numbers written in index form like 2^11 and 3^7 is not difficult, as any calculator would confirm that 2^11 = 2048 < 3^7 = 2187.
